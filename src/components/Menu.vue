d3619dff5c18328309b12f4abf479eb8b726f6fe
Farooq Asmaa <farooq.asmaa@gmail.com>
2018-06-19 12:03:38 +0200

    add navbar

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>
-    <h1>Menu</h1>
-  </div>
-
-  
+	<header class="header">
+		<ul class="main-nav">
+			<li><a href="#">Fonctionnalités</a></li>
+			<li><a href="#">Premium</a></li>
+			<li><a href="#">Blog</a></li>
+			<li>
+				<h1 class="logo"><a href="#"></a></h1>
+			</li>
+			<li><a href="#">FR</a></li>
+			<li><a href="#">Se connecter</a></li>
+			<li><a href="#" class="save-btn">S'enregistrer</a></li>
+		</ul>
+	</header>
 </template>
 
 <script>
