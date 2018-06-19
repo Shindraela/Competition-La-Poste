b17414becf4840512edc662d6cf692e20b371646
Farooq Asmaa <farooq.asmaa@gmail.com>
2018-06-19 14:02:35 +0200

    logo in center

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,16 +1,27 @@
 <template>
 	<header class="header">
-		<ul class="main-nav">
-			<li><a href="#">Fonctionnalités</a></li>
-			<li><a href="#">Premium</a></li>
-			<li><a href="#">Blog</a></li>
-			<li>
-				<div class="logo"><a href="#"></a></div>
-			</li>
-			<li><a href="#">FR</a></li>
-			<li><a href="#">Se connecter</a></li>
-			<li><a href="#" class="save-btn">S'enregistrer</a></li>
-		</ul>
+		<div>
+			<nav class="first-nav">
+				<ul class="main-nav">
+					<li><a href="#">Fonctionnalités</a></li>
+					<li><a href="#">Premium</a></li>
+					<li><a href="#">Blog</a></li>
+					<li>
+						<div class="logo"><a href="#"></a></div>
+					</li>
+				</ul>
+			</nav>
+		</div>
+
+		<div>
+			<nav class="second-nav">
+				<ul class="main-nav">
+					<li><a href="#">FR</a></li>
+					<li><a href="#">Se connecter</a></li>
+					<li><a href="#" class="save-btn">S'enregistrer</a></li>
+				</ul>
+			</nav>
+		</div>
 	</header>
 </template>
 
