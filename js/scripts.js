b1400a9acf9294bbf8ac16560636b86ffe8d47d6
dylanolv <oliveira.dylann@gmail.com>
2018-06-21 10:23:59 +0200

    Changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,33 @@
-(function () {
+// resize helpers
+function onResize() {
+  // auto resize header
+  $('#homepage').height($(window).height());
+}
 
-  // resize helpers
-  function onResize() {
-    // auto resize header
-    $('#homepage').height($(window).height());
-  }
+// scroll helpers
+function onScroll() {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  true;
+}
 
-  // scroll helpers
-  function onScroll() {
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+$(document).ready(function () {
 
-    $('body').toggleClass('scrolled', scrollTop > 0);
+  $(window).bind('resize', _.throttle(onResize, 100));
+  onResize();
 
-    true;
-  }
+  $(window).bind('scroll', _.throttle(onScroll, 100));
+  onScroll();
 
-  $(document).ready(function () {
-
-    $(window).bind('resize', _.throttle(onResize, 100));
-    onResize();
-
-    $(window).bind('scroll', _.throttle(onScroll, 100));
-    onScroll();
-
-    $(window).bind('scroll', function () {
-      var distance = 70;
-      if ($(window).scrollTop() > distance) {
-        $('.header').addClass('scrolled');
-        $('.header a').addClass('textscrolled');
-        $('.logo').addClass('logoscrolled');
-      } else {
-        $('.header').removeClass('scrolled');
-        $('.header a').removeClass('textscrolled');
-        $('.logo').removeClass('logoscrolled');
-      }
-    });
+  $(window).bind('scroll', function () {
+    var distance = 70;
+    if ($(window).scrollTop() > distance) {
+      $('.header').addClass('scrolled');
+      $('.header a').addClass('textscrolled');
+      $('.logo').addClass('logoscrolled');
+    } else {
+      $('.header').removeClass('scrolled');
+      $('.header a').removeClass('textscrolled');
+      $('.logo').removeClass('logoscrolled');
+    }
   });
-})();
+});
